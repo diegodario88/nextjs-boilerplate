dd4256add6d5c01134471c60cfa0d16ec99bc62c
Diego Dario <diegodario88@gmail.com>
2021-04-13 20:43:37 -0300

    feat: Creating the first styles

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles';
+
 export default function Main() {
   return (
-    <main>
-      <h1>Testing with Jest</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo src="/images/world-map.svg" alt="Imagem do mapa múndi" />
+      <S.Title>Testing with Jest</S.Title>
+      <S.Description>Typescript, ReactJS e Styled Components</S.Description>
+      <S.Illustration
+        src="/images/hero-illustration.svg"
+        alt="Desenvolvedor atuando em um computador"
+      />
+    </S.Wrapper>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,9 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next React - Boilerplate</title>
-        <link rel="shortcut icon" href="/images/icon-512.png" />
-        <link rel="apple-touch-icon" href="/images/icon-512.png" />
+        <title>Boilerplate</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" href="/images/favicon.ico" />
         <meta
           name="description"
           content="A simple project starter to work with NextJs, React, TypeScript and Styled Components"
